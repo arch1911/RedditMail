b3bc51e2e253582b276e91357d734d0c2f18bee4
Luke Zambella <lukezambella@gmail.com>
2016-04-13 11:58:37 -0400

    Added Login.py

--- RedditMail/Login.py
+++ RedditMail/Login.py
@@ -1,6 +1,5 @@
-__author__ = 'Luke Zambella'
-
-'''
+import RedditMail
+"""
 Function to return a list that contains the emails login info.
 The file should contain the following:
 USERNAME:[USERNAME]
@@ -8,22 +7,22 @@ PASSWORD:[PASSWORD]
 
 Replace the brackets with the correct info
 Note: The function currently does not account for encrypted strings or ciphers like base64 so it's insecure.
-'''
+"""
 
 
 def getInfo():
-    logFile = ''  # Replace this with the file to open
-    userName = ''
+    log_file = RedditMail.__log_file__  # Replace this with the file to open
+    user_name = ''
     password = ''
 
-    with open(logFile) as x:
+    with open(log_file) as x:
         data = x.readlines()
     for x in data:
         d = x.split(":")
         if d[0] == "USERNAME":
-            userName = (d[1])
-            userName = userName.strip()
+            user_name = (d[1])
+            user_name = user_name.strip()
         elif d[0] == "PASSWORD":
             password = str(d[1])
             password = password.strip()
-    return {userName, password}
+    return user_name, password
