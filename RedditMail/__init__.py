4578701f66d2386ca6dc417e02908e05b24c4ea0
Luke Zambella <lukezambella@gmail.com>
2016-04-13 11:59:42 -0400

    Added global variables.

--- RedditMail/__init__.py
+++ RedditMail/__init__.py
@@ -1,1 +1,7 @@
+# I put global variables here
 __author__ = 'Luke Zambella'
+__version__ = '3.0'
+__imapserver__ = 'imap.gmail.com'
+__smtpserver__ = 'smtp.gmail.com'
+__smtp_port__ = '587'
+__log_file__ = 'login.txt'
